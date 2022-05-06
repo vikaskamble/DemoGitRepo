ec367eb615e319d5ce758cab6b97d0840b1ae7eb
vikas <vikas.kamble@afourtech.com>
2022-05-06 05:49:40 -0700

    test

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -4,6 +4,7 @@ public class LoginPage{
 
     public void loginPage() {
         System.out.println( "Login Page." );
+        System.out.println( "Login Page." );
     }
 
     public void loginSet(){
