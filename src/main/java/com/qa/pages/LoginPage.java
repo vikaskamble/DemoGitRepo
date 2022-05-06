15ce4b3fb46a4bc51c39c15a579e06dd3d2a5e78
vikas <vikas.kamble@afourtech.com>
2022-05-06 05:43:58 -0700

    Search Page feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -2,9 +2,13 @@ package com.qa.pages;
 
 public class LoginPage{
 
-public void loginPage(){
-    System.out.println( "Login Page." );
-}
+    public void loginPage() {
+        System.out.println( "Login Page." );
+    }
+
+    public void loginSet(){
+        System.out.println( "Login set method." );
+    }
 
 }
 
